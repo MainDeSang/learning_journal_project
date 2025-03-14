b12af20215efcb0e6bf147a0ad6cfcfd0f25210d
johann <johann.lindele@tn.mischok.academy>
2025-03-14 14:17:37 +0100

    @ManyToMany relationship added

--- src/main/java/com/example/learning_journal_project/model/Topic.java
+++ src/main/java/com/example/learning_journal_project/model/Topic.java
@@ -2,6 +2,8 @@ package com.example.learning_journal_project.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Set;
+
 @Entity //→ Markiert diese Klasse als eine JPA-Entität
 @Table(name = "topics") //→ Markiert diese Klasse als eine Tabelle im Datenbank
 @Getter //→ Erzeugt automatisch Getter- und Setter-Methoden
@@ -20,4 +22,10 @@ public class Topic {
     private String title;
     private String description;
     private String duration;
+
+
+    //Definiert die Beziehung als bidirektional.
+    @ManyToMany(mappedBy = "topics") //Das Feld "topics" in der User-Klasse wird als "users" in der Topic-Klasse gemappt. & mappedBy=Bedeutet, dass die User-Entität die Beziehung steuert (führt).
+    private Set<User> users;//Enthält die User, die dieses Topic gewählt haben.
+
 }
--- src/main/java/com/example/learning_journal_project/model/User.java
+++ src/main/java/com/example/learning_journal_project/model/User.java
@@ -3,6 +3,9 @@ package com.example.learning_journal_project.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity //→ Markiert diese Klasse als eine JPA-Entität
 @Table(name = "users") //→ Markiert diese Klasse als eine Tabelle im Datenbank
 @Getter //→ Erzeugt automatisch Getter- und Setter-Methoden
@@ -32,5 +35,14 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
+
+    //Definiert eine Many-to-Many-Beziehung zwischen User und Topic.
+    @ManyToMany
+    @JoinTable(
+            name = "user_topic", // Bestimmt den Namen der Zwischentabelle
+            joinColumns = @JoinColumn(name = "user_id"), // Verknüpfung zur User-Tabelle & erstellt eine FK-Spalte (user_id) in der Zwischentabelle.
+            inverseJoinColumns = @JoinColumn(name = "topic_id") // Verknüpfung zur Topic-Tabelle & erstellt eine FK-Spalte (topic_id) in der Zwischentabelle.
+    )
+    private Set<Topic> topics = new HashSet<>(); // Verwenden einer Set-Menge zur Vermeidung doppelter Einträge
 
 }
